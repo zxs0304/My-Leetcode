08f98151c1691d74b1a6f5459801f19ea06c021c
zhangxisen <>
2024-11-26 21:03:01 +0800

    update

---
+++ C#LeetCode/C#LeetCode/219存在重复元素II(哈希表).cs
@@ -0,0 +1,41 @@
+﻿
+namespace C_LeetCode
+{
+    /*219. 存在重复元素 II
+给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且 abs(i - j) <= k 。如果存在，返回 true ；否则，返回 false 。
+输入：nums = [1,2,3,1], k = 3
+输出：true
+输入：nums = [1,0,1,1], k = 1
+输出：true
+输入：nums = [1,2,3,1,2,3], k = 2
+输出：false
+
+	 */
+    public class _219存在重复元素II_哈希表_
+	{
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            // 键是元素，值是最近一次出现的位置
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!dic.ContainsKey(nums[i]))
+                {
+                    dic.Add(nums[i], i);
+                }
+                else
+                {
+                    int preIndex = dic[nums[i]];
+                    if (i - preIndex <= k)
+                    {
+                        return true;
+                    }
+                    // 更新最近一次出现的位置
+                    dic[nums[i]] = i;
+                }
+            }
+            return false;
+        }
+    }
+}
+
---
+++ C#LeetCode/C#LeetCode/3206交替组.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+namespace C_LeetCode
+{
+    /*3206. 交替组 I
+给你一个整数数组 colors ，它表示一个由红色和蓝色瓷砖组成的环，第 i 块瓷砖的颜色为 colors[i] ：
+colors[i] == 0 表示第 i 块瓷砖的颜色是 红色 。colors[i] == 1 表示第 i 块瓷砖的颜色是 蓝色 。
+环中连续 3 块瓷砖的颜色如果是 交替 颜色（也就是说中间瓷砖的颜色与它 左边 和 右边 的颜色都不同），那么它被称为一个 交替 组。
+请你返回 交替 组的数目。
+注意 ，由于 colors 表示一个 环 ，第一块 瓷砖和 最后一块 瓷砖是相邻的。
+输入：colors = [1,1,1]
+输出：0
+输入：colors = [0,1,0,0,1]
+输出：3
+	 */
+    public class _206交替组
+	{
+        public int NumberOfAlternatingGroups(int[] colors)
+        {
+            int n = colors.Length;
+            int result = 0;
+            for (int i = 0; i< n; i++)
+            {
+                int nextIndex = (i + 1)%n;
+                int next_nextIndex = (i + 2)%n;
+                if (colors[i] != colors[nextIndex] && colors[i] == colors[next_nextIndex])
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+    }
+}
+
