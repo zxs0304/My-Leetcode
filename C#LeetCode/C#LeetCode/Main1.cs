2eba4e3e6980e4e37d2655158e8ed77c8c0755b0
zxs <1376260231@qq.com>
2024-12-12 23:15:00 +0800

    update

--- C#LeetCode/C#LeetCode/Main1.cs
+++ C#LeetCode/C#LeetCode/Main1.cs
@@ -7,39 +7,37 @@ using System.Threading.Tasks;
 
 namespace C_LeetCode
 {
-
-    class Student
+    public delegate void myAction();
+    class Person2
     {
+        public myAction action;
 
+        public void SetMyAction(myAction a)
+        {
+            action = a;
+        }
+
+    }
+
+    class Person1
+    {
+        public void MyPrint()
+        {
+            Console.WriteLine("Person1的函数");
+        }
 
         public static void Main(string[] args)
         {
-            _152乘积最大的子数组_dp_ t = new _152乘积最大的子数组_dp_();
+            Person1 person1 = new Person1();
+            Person2 person2 = new Person2();
+            person2.SetMyAction(person1.MyPrint);
+            person2.action.Invoke();
+            
 
-            var result = t.MaxProduct1(new int[] {-4 ,-3,-2});
-            Console.WriteLine(result);
+            //_152乘积最大的子数组_dp_ t = new _152乘积最大的子数组_dp_();
+
+            //var result = t.MaxProduct1(new int[] {-4 ,-3,-2});
+            //Console.WriteLine(result);
         }
-
-        public int Rob(int[] nums)
-        {
-            int n = nums.Length;
-            int[] dp = new int[n+1];
-            dp[0] = nums[0];
-
-            for (int i = 1;i<n;i++)
-            {
-                if (i == 1)
-                {
-                    dp[1] = Math.Max(dp[0], 0 + nums[1]);
-                }
-                else
-                {
-                    dp[i] = Math.Max(dp[i - 1], dp[i - 2] + nums[i]);
-                }
-
-            }
-
-        }
-
     }
 }
